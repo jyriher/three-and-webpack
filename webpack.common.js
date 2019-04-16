d2695e7eb374d29b9c589c2e603185dd757c94c1
jyriher <jyri.herka@metropolia.fi>
2019-04-16 08:22:43 +0300

    adding content

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const CopyPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+// add to plugins array:
+
+
 module.exports = {
   entry: {
     app: './src/index.js',
@@ -14,8 +19,16 @@ module.exports = {
         viewport: 'width=device-width, user-scalable=no, initial-scale=1.0'
       },
       template: './src/index.html'
-    })
-  ],
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new WriteFilePlugin(),
+    new CopyPlugin([
+    {
+        from: 'models/',
+        to: 'models/',
+        context: 'src/',
+    },
+  ]) ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
